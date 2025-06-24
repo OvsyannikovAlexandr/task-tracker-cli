972561d98ea912998d47b7254ae94a367c6d20e2
OvsyannikovAleksandr <ovsyannikov.alv98@gmail.com>
2025-06-24 14:37:01 +0300

    ListTasks test

--- internal/service/task_test.go
+++ internal/service/task_test.go
@@ -1,11 +1,30 @@
 package service_test
 
 import (
+	"bytes"
+	"io"
+	"os"
+	"strings"
 	"task-traker-cli/internal/model"
 	"task-traker-cli/internal/service"
 	"testing"
 	"time"
 )
+
+func captureOutput(f func()) string {
+	old := os.Stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+
+	f()
+
+	w.Close()
+	var buf bytes.Buffer
+	io.Copy(&buf, r)
+	os.Stdout = old
+
+	return buf.String()
+}
 
 func TestGetNextID(t *testing.T) {
 	tasks := []model.Task{
@@ -67,4 +86,56 @@ func TestSaveAndLoadTasks(t *testing.T) {
 	if len(loaded) != 1 || loaded[0].Description != "Test" {
 		t.Error("loaded tasks do not match saved tasks")
 	}
+}
+
+func TestListTasks(t *testing.T) {
+	tasks := []model.Task{
+		{TaskID: 1, Description: "Task1", Status: "todo", CreatedAt: time.Now(), UpdatedAt: time.Now()},
+		{TaskID: 2, Description: "Task2", Status: "in-progress", CreatedAt: time.Now(), UpdatedAt: time.Now()},
+		{TaskID: 3, Description: "Task3", Status: "done", CreatedAt: time.Now(), UpdatedAt: time.Now()},
+	}
+
+	t.Run("list all tasks", func(t *testing.T) {
+		output := captureOutput(func() {
+			service.ListTasks(tasks, "")
+		})
+		if !strings.Contains(output, "Task1") ||
+			!strings.Contains(output, "Task2") ||
+			!strings.Contains(output, "Task3") {
+			t.Error("listTasks should output all tasks")
+		}
+	})
+
+	t.Run("filter todo", func(t *testing.T) {
+		output := captureOutput(func() {
+			service.ListTasks(tasks, "todo")
+		})
+		if !strings.Contains(output, "Task1") ||
+			strings.Contains(output, "Task2") ||
+			strings.Contains(output, "Task3") {
+			t.Error("listTasks should output only 'todo' tasks")
+		}
+	})
+
+	t.Run("filter in-progress", func(t *testing.T) {
+		output := captureOutput(func() {
+			service.ListTasks(tasks, "in-progress")
+		})
+		if !strings.Contains(output, "Task2") ||
+			strings.Contains(output, "Task1") ||
+			strings.Contains(output, "Task3") {
+			t.Error("listTasks should output only 'in-progress' tasks")
+		}
+	})
+
+	t.Run("filter done", func(t *testing.T) {
+		output := captureOutput(func() {
+			service.ListTasks(tasks, "done")
+		})
+		if !strings.Contains(output, "Task3") ||
+			strings.Contains(output, "Task2") ||
+			strings.Contains(output, "Task1") {
+			t.Error("listTasks should output only 'done' tasks")
+		}
+	})
 }
